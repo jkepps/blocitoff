6ecf1ef61ef74d2120b2d942ea46739176280c46
Jordan Epps <jordan.k.epps@gmail.com>
2015-10-12 12:24:08 -0700

    Username attribute added to users.

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20151012044717) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
