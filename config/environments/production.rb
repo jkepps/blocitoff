f22999879e3380ff548ae0b0050f980e14b2f79b
Jordan Epps <jordan.k.epps@gmail.com>
2016-01-12 13:57:16 -0800

    changed app name in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'limitless-cliffs-7354.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'blocitoff-jepps.herokuapp.com' }
 end
